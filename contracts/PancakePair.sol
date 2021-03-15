d416a14b8fc775659ac6eb5ea9ff8f378fca2704
Binzy <xbinzy@protonmail.com>
2021-03-14 17:13:55 -0700

    build: Binzy doz it

--- contracts/PancakePair.sol
+++ contracts/PancakePair.sol
@@ -6,7 +6,7 @@ import './libraries/Math.sol';
 import './libraries/UQ112x112.sol';
 import './interfaces/IERC20.sol';
 import './interfaces/IPancakeFactory.sol';
-import './interfaces/IArcaneCallee.sol';
+import './interfaces/IPancakeCallee.sol';
 
 contract PancakePair is IPancakePair, PancakeERC20 {
     using SafeMath  for uint;
@@ -169,7 +169,7 @@ contract PancakePair is IPancakePair, PancakeERC20 {
         require(to != _token0 && to != _token1, 'Arcane: INVALID_TO');
         if (amount0Out > 0) _safeTransfer(_token0, to, amount0Out); // optimistically transfer tokens
         if (amount1Out > 0) _safeTransfer(_token1, to, amount1Out); // optimistically transfer tokens
-        if (data.length > 0) IArcaneCallee(to).arcaneCall(msg.sender, amount0Out, amount1Out, data);
+        if (data.length > 0) IPancakeCallee(to).arcaneCall(msg.sender, amount0Out, amount1Out, data);
         balance0 = IERC20(_token0).balanceOf(address(this));
         balance1 = IERC20(_token1).balanceOf(address(this));
         }
